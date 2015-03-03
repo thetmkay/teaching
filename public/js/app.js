4197aea9455571301d62c0c892c41d8fb0f62066
George Nishimura <thetmkay@gmail.com>
2015-03-03 00:15:48 +0000

    update index page and added tasks

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,19 @@
-(function($) {
+(function() {
 
 	'use strict';
 
-})(jQuery);
+      document.addEventListener('DOMContentLoaded', function() {
+          var includes = document.querySelectorAll('.includes-link');
+          for(var i = 0; i < includes.length; i++) {
+             includes[i].addEventListener('click', function(e) {
+              e.preventDefault();
+              var example_box = this.parentElement.parentElement;
+              example_box.classList.toggle('hide-includes');
+             })
+          }
+
+      })
+
+
+
+})();
